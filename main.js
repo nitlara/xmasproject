07941f94039b00861ca3dd9ab9520d9d40ec24d5
nitlara <nitlara@gmail.com>
2020-12-21 18:43:28 +0000

    añadido en comentarios opciones para tirar y array opciones

--- main.js
+++ main.js
@@ -19,11 +19,29 @@ let gameBoard = [
 
 
 //Fire via -->  onClick to every <div> and call the fireTorpedo function with the coordinates of the div. 
-  window.onload = function loadFn() {
-	var firepoint = event.target;
-    console.log(firepoint.tagName);
-    };
-
+ 
+window.onload = function() {
+     let opciones = [
+    {
+      name: "agua",
+      color: "black",
+      value: 0},
+    {
+        name: "tocado",
+        color:"red",
+        value:1},
+    {
+        name="hundido",
+        color:"black",
+        value:2}
+     ]
+/*
+  var newColor = document.getElementsById("casilla");
+  newColor.classList.add("waterspace");
+newColor[0].innerHTML = "AGUA";
+ newColor.classList.add(variable[posicion].color);
+  newColor[añadir array nueva] = variable[posicion].value;
+};*/
     //function fireTorpedo
      // onClick="fireTorpedo(${la columna que sea},${la fila que sea}, evento
 //Replace the value on the gameBoard and render the board again.
@@ -35,3 +53,5 @@ let gameBoard = [
     //señala donde estan los barcos enmarcando casilla de color naranja
 //Botón reinicio partida 
     // colocar barcos
+
+    
