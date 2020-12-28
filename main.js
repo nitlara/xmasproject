29d9aef4452a15085ea3c56a5934e52c47149740
nitlara <nitlara@gmail.com>
2020-12-28 17:11:29 +0000

    cambia color de la casilla seleccionada segun si es agua o barco

--- main.js
+++ main.js
@@ -25,9 +25,9 @@ function myFunction (event) {
     var target = event.target.className;
     if (target.indexOf('casillas ') === 0 ){     
         // genera 3 variables dentro de la función con substring de la clase, con la cual obtenemos datos de la posción al estar en las clases -->
-        target = target.substring(9,11)
-            targetLetter = target.substring(0,1)
-            targetNumber = target.substring(1,2)
+        targetB = target.substring(9,11)
+            targetLetter = targetB.substring(0,1)
+            targetNumber = targetB.substring(1,2)
         //Conversión para posición Numero y Letra en el Array
             var targetPosition = targetNumber -1;
             var targetLposition =  0;
@@ -53,20 +53,16 @@ function myFunction (event) {
             console.log(valorgameboard);
         //Cambiar clase y valor matriz
             // clase        
-               /* if (valorgameboard == 0){
-                    target.classList.add("waterspace");
-                   console.log(target);
-                }*/
-              
-                /*Si es 0 ++3; añade clase "waterspace"
-                if (target.value == 0) {   
-                    ++3,             
-                    classList.add("waterspace");}
-                //Si es 1 ++1; añade clase "touchspace"
-                if (target.value == 0) {   
-                    ++1,             
-                    classList.add("touchspace");}
-*/     } //este cierre viene de "if (target.indexOf('casillas ') === 0 )"
+                if (valorgameboard == 0){
+                  var elementos = document.getElementsByClassName(targetB);
+                    elementos[0].classList.add("waterspace");
+                }
+                 if (valorgameboard == 1){
+                  var elementos = document.getElementsByClassName(targetB);
+                    elementos[0].classList.add("touchspace");
+                }
+
+            } //este cierre viene de "if (target.indexOf('casillas ') === 0 )"
 
     } //este cierre cierra myfunction 
 
