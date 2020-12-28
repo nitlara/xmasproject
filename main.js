6fb0c990d16d4f0a4c6eec6fe1915d2ab1ff0430
nitlara <nitlara@gmail.com>
2020-12-28 17:50:17 +0000

    añadir cambio valores array

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 window.onload = function Addonclick() {
     var idcasillas = document.getElementsByClassName("casillas");
     for (var i = 0; i < idcasillas.length; i++) {
-        this.addEventListener("click", myFunction, false);
+        idcasillas[i].addEventListener("click", myFunction, false);
     }
 }
 // Valores barcos
@@ -23,46 +23,56 @@ let gameBoard = [
 // Función que genera el evento TARGET de las clases "casilla"
 function myFunction (event) {
     var target = event.target.className;
-    if (target.indexOf('casillas ') === 0 ){     
-        // genera 3 variables dentro de la función con substring de la clase, con la cual obtenemos datos de la posción al estar en las clases -->
+    //duplica array dentro la función
+    var gameBoardB = gameBoard;
+    // genera 3 variables dentro de la función con substring de la clase, con la cual obtenemos datos de la posción al estar en las clases -->
         targetB = target.substring(9,11)
             targetLetter = targetB.substring(0,1)
             targetNumber = targetB.substring(1,2)
-        //Conversión para posición Numero y Letra en el Array
-            var targetPosition = targetNumber -1;
-            var targetLposition =  0;
-                 if (targetLetter == "A") {
-                    targetLposition = 0;
-                    } else if (targetLetter == "B") {
-                    targetLposition = 1;
-                    } else if (targetLetter == "C") {
-                    targetLposition = 2;
-                    } else if (targetLetter == "D") {
-                    targetLposition = 3;
-                    } else if (targetLetter == "E") {
-                    targetLposition = 4;
-                    } else if (targetLetter == "F") {
-                    targetLposition = 5;
-                    } else if (targetLetter == "G") {
-                    targetLposition = 6;
-                    } else {targetLposition = 7;}
-        //Obtener valor de las coordenadas
-                //Tabla del array gameBoard
-                //console.table(gameBoard);
-            var valorgameboard = (gameBoard[targetLposition][targetPosition]);
-        //Cambiar clase y valor matriz
-            // clase    
-            var elementos = document.getElementsByClassName(targetB);    
-                if (valorgameboard == 0){
-                  elementos[0].classList.add("waterspace");
-                }
+                //Conversión para posición Numero y Letra en el Array
+                var targetPosition = targetNumber -1;
+                var targetLposition =  0;
+                    if (targetLetter == "A") {
+                        targetLposition = 0;
+                        } else if (targetLetter == "B") {
+                        targetLposition = 1;
+                        } else if (targetLetter == "C") {
+                        targetLposition = 2;
+                        } else if (targetLetter == "D") {
+                        targetLposition = 3;
+                        } else if (targetLetter == "E") {
+                        targetLposition = 4;
+                        } else if (targetLetter == "F") {
+                        targetLposition = 5;
+                        } else if (targetLetter == "G") {
+                        targetLposition = 6;
+                        } else {targetLposition = 7;}
+    //Obtener valor de las coordenadas
+        //console.table(gameBoard);//Tabla del array gameBoard
+        var valorgameboard = gameBoard[targetLposition][targetPosition];
+    //Cambiar clase 
+        var elementos = document.getElementsByClassName(targetB);    
+            if (valorgameboard == 0){
+                elementos[0].classList.add("waterspace");
+                    // pruebas para cambiar valores array
+                    gameBoard[targetLposition][targetPosition] = 3;
+                    }
                 else if (valorgameboard == 1){
                     elementos[0].classList.add("touchspace");
-                }
-            } //este cierre viene de "if (target.indexOf('casillas ') === 0 )"
-    } //este cierre cierra myfunction 
+                    gameBoard[targetLposition][targetPosition] = 2;
+                    }                  
+                //console.table(gameBoard);
+    } //cierra myfunction 
 
 
 
+ //Ampliar con: 
+    //Dentro de myFunction
+        // Entrar "target" por promt
+        
+    // Button--> Mostrar barcos
+    // Button--> Resetear matriz aleatoriamente
 
+//Dudas
+    //Generar toda la tabla, con las clases des de js.
 
