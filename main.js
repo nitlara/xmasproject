b9af7f4e5920ad5750ceb4cc1063b4d3ecc1b0a2
nitlara <nitlara@gmail.com>
2020-12-29 15:23:39 +0000

    divisón en 3 funciones más conversión al inglés funciones y variables. Pendiente afinar engranaje

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-// Añadir addEventListener al CLICK
+//Al cargar window añade un evento al hacer click en las casillas con clase "casillas" y ejecuta la función myFunctionEvent
 window.onload = function Addonclick() {
-    var idcasillas = document.getElementsByClassName("casillas");
-    for (var i = 0; i < idcasillas.length; i++) {
-        idcasillas[i].addEventListener("click", myFunction, false);
+    var idboxes = document.getElementsByClassName("boxes");
+    for (var i = 0; i < idboxes.length; i++) {
+        idboxes[i].addEventListener("click", myFunctionEvent, false);
+    }
+    //Al cargar window añade un evento al hacer click en el botón con id #shoot que dispara un prompt y ejecuta myFunctionButton
+    var shoot = document.querySelector('#shoot');
+    shoot.addEventListener('click', ()=> (promptMe()));
+        function promptMe() {
+        var battleshoot = prompt("Insert your shoot", "B1");
+        myFunctionButton;
     }
 }
 // Valores barcos
@@ -20,19 +27,27 @@ let gameBoard = [
     [0,0,0,0,0,0,0,0,0],
     [1,1,1,1,0,0,0,0,0]
 ];
-
-
-// Función que genera el evento TARGET de las clases "casilla"
-function myFunction (event) {
+// Función que genera el evento TARGET de las clases "casilla" y llama la siguiente función.
+function myFunctionEvent (event) {
     var target = event.target.className;
-    //duplica array dentro la función
-    var gameBoardB = gameBoard;
-    // genera 3 variables dentro de la función con substring de la clase, con la cual obtenemos datos de la posción al estar en las clases -->
-        targetB = target.substring(9,11)
+    // genera 1 variable dentro de la función con substring de la clase para quitar "casilla"
+        targetB = target.substring(9,11);
+        //llama la siguiente función necesaria
+       ()=> (TranslatePosition(targetB));
+    }
+//Función button añade valor para activar la siguiente función
+function myFunctionButton (){
+    var targetB = battleshoot; //añadir la conversión posible del codigo introducido a mano
+    //llama la siguiente función necesaria
+    ()=> (TranslatePosition(targetB));
+}
+//Función que convierte los datos entrados (por click o button) en las coordenadas del array y hace los cambios necesarios de clase y valores del array.
+function TranslatePosition (){  
             targetLetter = targetB.substring(0,1)
             targetNumber = targetB.substring(1,2)
-                //Conversión para posición Numero y Letra en el Array
+                //Conversión para posición dada de targetNumber en el array gameBoard
                 var targetPosition = targetNumber -1;
+                //Conversión para posición dada de targetLetter en el array gameBoard
                 var targetLposition =  0;
                     if (targetLetter == "A") {
                         targetLposition = 0;
@@ -50,46 +65,48 @@ function myFunction (event) {
                         targetLposition = 6;
                         } else {targetLposition = 7;}
     //Obtener valor de las coordenadas
-        //console.table(gameBoard);//Tabla del array gameBoard
+        //console.table(gameBoard);//Tabla del array gameBoard. Dejar comentado si no es necesario hacer comprovaciones
         var valorgameboard = gameBoard[targetLposition][targetPosition];
     //Cambiar clase 
         var elementos = document.getElementsByClassName(targetB);    
             if (valorgameboard == 0){
                 elementos[0].classList.add("waterspace");
-                    // pruebas para cambiar valores array
+                    // cambiar valores array
                     gameBoard[targetLposition][targetPosition] = 3;
                     }
                 else if (valorgameboard == 1){
                     elementos[0].classList.add("touchspace");
                     gameBoard[targetLposition][targetPosition] = 2;
                     }                  
-                //console.table(gameBoard);
-    } //cierra myfunction 
-
-
-//button problemas tiempo/orden
-    console.log("1");
-setTimeout(function(){
-var foo = document.getElementById('shoot');
-console.log("2",foo);
-foo.addEventListener('click', promptMe);
-function promptMe() {
-    var battleshoot = prompt("Insert your shoot", "B1");
-    alert (battleshoot);
-}
-},1000)
-
-
-
+                //console.table(gameBoard);//Tabla del array gameBoard. Dejar comentado si no es necesario hacer comprovaciones
+    } //cierra TranslatePosition
 
 
  //Ampliar con: 
     //Dentro de myFunction
         // Entrar "target" por promt
-        
     // Button--> Mostrar barcos
     // Button--> Resetear matriz aleatoriamente
-
 //Dudas
     //Generar toda la tabla, con las clases des de js.
-    //Tiempo de lag js vs html --> ejemplo button
+/*    for (let i=0; i < gameboard.length ;i++)
+        CREA DIV con clase ROW
+        var newDivrow = document.createElement("div");
+        newDivrow.classList.add("Row")
+
+        {for (let h = 0; h < 10; h++)
+            CREA DIV con clase casillas 
+            var newDivcasillas = document.createElement("div");
+            newDivcasillas.classList.add("boxes")
+            //aquí appendchild
+
+            {for (let j = 0; j < 10; j++)
+                CREA clase con claseletra[0]+ clasenumero++1
+                var claseletra = ['A','B','C','D'];
+                var clasenumero = 0
+                 var claseposicion = claseletra[0] + clasenumero
+                
+                
+                var newDivcasillas.classList.add(claseletra)
+                Si j == 9 elimina valor 0 array 
+*/
